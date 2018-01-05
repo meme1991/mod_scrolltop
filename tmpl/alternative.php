d3f2cfeff93d0c26763da16f25cde325f13371b2
Emanuele Cappato <e.kappato@icloud.com>
2018-01-05 09:28:52 +0100

    Update alternative.php

--- tmpl/alternative.php
+++ tmpl/alternative.php
@@ -13,7 +13,7 @@ $document->addStyleSheet(JUri::base(true).'/modules/'.$module->module.'/css/scro
 ?>
 <div class="scroll-top-block" style="<?php echo $scrolltopPosition ?>: 30px">
   <span class="fa-stack fa-lg" style="color: <?php echo $scrolltopBgColor ?>">
-    <i class="fa fa-square fa-stack-2x"></i>
-    <i class="fa fa-chevron-up fa-stack-1x fa-inverse" style="color: <?php echo $scrolltopColor ?>"></i>
+    <i class="fas fa-square fa-stack-2x"></i>
+    <i class="far fa-chevron-up fa-stack-1x fa-inverse" style="color: <?php echo $scrolltopColor ?>"></i>
   </span>
 </div>
